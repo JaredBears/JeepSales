8c3a9499ba63ce0dbc8aa93760dffb8fe6f6311f
Jared Bears <jaredbears@outlook.com>
2022-08-06 18:40:48 -0500

    Sixth Commit Week 14, video 3 complete src/main/java/com/promineotech/jeep/controller/DefaultJeepSalesController.java src/main/java/com/promineotech/jeep/service/DefaultJeepSalesService.java src/main/java/com/promineotech/jeep/service/JeepSalesService.java

--- src/main/java/com/promineotech/jeep/controller/DefaultJeepSalesController.java
+++ src/main/java/com/promineotech/jeep/controller/DefaultJeepSalesController.java
@@ -1,18 +1,23 @@
 package com.promineotech.jeep.controller;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import com.promineotech.jeep.entity.Jeep;
+import com.promineotech.jeep.service.JeepSalesService;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
 public class DefaultJeepSalesController implements JeepSalesController {
 
+  @Autowired
+  private JeepSalesService jeepSalesService;
+  
   @Override
   public List<Jeep> fetchJeeps(String model, String trim) {
     log.info("Model={}, trim={}", model, trim);
-    return null;
+    return jeepSalesService.fetchJeeps(model, trim);
   }
 
 }
