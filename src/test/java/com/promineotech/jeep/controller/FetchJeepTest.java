f9d44ab75587189b4baa7477f0e7298b92726e4f
Jared Bears <jaredbears@outlook.com>
2022-08-06 20:08:40 -0500

    Seventh Commit Week 14 assignment complete src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java src/test/java/com/promineotech/jeep/controller/support/BaseTest.java

--- src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
+++ src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
@@ -1,39 +1,41 @@
 package com.promineotech.jeep.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.jdbc.JdbcTestUtils;
 import com.promineotech.jeep.controller.support.FetchJeepTestSupport;
 import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
-import lombok.Getter;
 
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-@Sql(scripts = {"classpath:flyway/migrations/V1.0_Jeep_Schema.sql",
-    "classpath:flyway/migrations/V1.1_Jeep_Data.sql"}, config = @SqlConfig(encoding = "utf-8"))
+@Sql(scripts = {"classpath:flyway/migrations/V1.0__Jeep_Schema.sql",
+    "classpath:flyway/migrations/V1.1__Jeep_Data.sql"}, config = @SqlConfig(encoding = "utf-8"))
 class FetchJeepTest extends FetchJeepTestSupport {
 
-  @LocalServerPort
-  private int serverPort;
-
   @Autowired
-  @Getter
-  private TestRestTemplate restTemplate;
+  private JdbcTemplate jdbcTemplate;
+
+  @Disabled
+  @Test
+  void testDb() {
+    int numrows = JdbcTestUtils.countRowsInTable(jdbcTemplate, "customers");
+    System.out.println("num=" + numrows);
+  }
 
   @Test
   void testThatJeepsAreReturnedWhenAValidModelAndTrimAreSupplied() {
@@ -43,12 +45,12 @@ class FetchJeepTest extends FetchJeepTestSupport {
     String uri = String.format("%s?model=%s&trim=%s", getBaseUri(), model, trim);
 
     // When: a connection is made to the URI
-    ResponseEntity<List<Jeep>> response =
-        restTemplate.exchange(uri, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+    ResponseEntity<List<Jeep>> response = getRestTemplate().exchange(uri, HttpMethod.GET, null,
+        new ParameterizedTypeReference<>() {});
 
     // Then: a success (OK = 200) status code is returned
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-    
+
     // And: the actual list returned is the same as the expected list;
     List<Jeep> expected = buildExpected();
     assertThat(response.getBody()).isEqualTo(expected);
--- src/test/java/com/promineotech/jeep/controller/support/BaseTest.java
+++ src/test/java/com/promineotech/jeep/controller/support/BaseTest.java
@@ -1,4 +1,4 @@
-package com.promineotech.jeep.controller.support;
+  package com.promineotech.jeep.controller.support;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
